1bef05a06a1d1d10fd26e1390d46c1eafea4238e
NehaK745 <nehakumari1461@gmail.com>
2021-06-20 06:20:18 +0530

    Added migration

--- migrations/2_final_migration.js
+++ migrations/2_final_migration.js
@@ -3,9 +3,9 @@ var DappToken = artifacts.require("./DogeToken.sol");
 var DappTokenSale = artifacts.require("./DogeCrowdsale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000).then(function() {
+  deployer.deploy(DogeToken, 1000000).then(function() {
     // Token price is 0.001 Ether
     var tokenPrice = 1000000000000000;
-    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+    return deployer.deploy(DogeCrowdsale, DogeToken.address, tokenPrice);
   });
 };
